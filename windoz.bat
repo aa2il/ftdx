2bff3ea981f1a405777387d127ace9c5ee217ca9
aa2il <jattili@yahoo.com>
2023-06-07 09:49:00 -0700

    Updates

---
+++ windoz.bat
@@ -0,0 +1,13 @@
+@echo off
+echo.
+echo Notes about how to run ftdx rig control gui on Windoze 10
+echo.
+echo Havent really execised this yet but seems ok so far
+echo    ftdx.py
+echo.
+echo To compile (Haven't tried this yet)
+echo.
+echo         pyinstaller --onefile ftdx.py
+echo         dist\ftdx.exe
+echo.
+
